a4f07b3562f7bf2dc1f9787a5c47952a6a2a11e8
Pankrat9v <31805018+spankratov1906@users.noreply.github.com>
2022-07-17 20:32:38 +0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme");
+
+    if (theme.getAttribute("href") == "light.css") {
+        theme.href = "dark.css"
+    } else {
+        theme.href = "light.css"
+    }
+}
+
+let switchLogo = document.getElementById("logo");
